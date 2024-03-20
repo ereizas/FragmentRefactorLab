b86500625ea7011f3dd908f8718f6c9082fa7cc8
Eric Reizas <tuo17224@temple.edu>
2024-03-20 15:46:39 -0400

    Prevented stacking of additional fragments by checking whether instance state was saved

--- app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
@@ -7,8 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .add(R.id.nameDisplayFragmentContainer,NameDisplayFragment())
-            .commit()
+        if(savedInstanceState==null)
+        {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.nameDisplayFragmentContainer,NameDisplayFragment())
+                .commit()
+        }
+
     }
 }
